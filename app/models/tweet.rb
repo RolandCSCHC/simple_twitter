5cf94d62848da505cfe6528e1cfe921f8f919322
RolandCSCHC <rcschlosser@miuandes.cl>
2023-06-10 17:48:41 -0400

    Hashtags implemented

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,17 @@ class Tweet < ApplicationRecord
     belongs_to :user
     has_many :tags
 
+    attribute :hashtags
+
+    after_create :create_tags
+
+    def create_tags
+        hashtags.split(",").each do |h|
+            hashtag = Hashtag.find_or_create_by name: h
+            Tag.create tweet: self, hashtag: hashtag
+        end
+    end
+
     def creation_date
         created_at.strftime("%e %b %Y")
     end
